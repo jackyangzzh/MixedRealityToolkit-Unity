919b94d02aaa487e859c0edf454d1c700ccc0a47
RogPodge <roliu@microsoft.com>
2021-04-12 17:48:15 -0700

    Data providers now only load if they match the target pipeline, also created XRSettingsUtilities for non-editor scenarios

--- Assets/MRTK/Core/Utilities/Editor/XRSettingsEditorUtilities.cs
+++ Assets/MRTK/Core/Utilities/Editor/XRSettingsEditorUtilities.cs
@@ -15,10 +15,10 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Editor
     /// Utilities that abstract XR settings functionality so that the MRTK need not know which
     /// implementation is being used.
     /// </summary>
-    public static class XRSettingsUtilities
+    public static class XRSettingsEditorUtilities
     {
-#if UNITY_2019_3_OR_NEWER
-        static XRSettingsUtilities()
+#if UNITY_2019_3_OR_NEWER && UNITY_EDITOR
+        static XRSettingsEditorUtilities()
         {
             // Called when packages are installed or uninstalled
             EditorApplication.projectChanged += EditorApplication_projectChanged;
@@ -86,7 +86,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Editor
             }
         }
 
-#if UNITY_2019_3_OR_NEWER
+#if UNITY_2019_3_OR_NEWER && UNITY_EDITOR
         /// <summary>
         /// Called when packages are installed or uninstalled, to toggle a new check on XR SDK package installation status.
         /// </summary>
